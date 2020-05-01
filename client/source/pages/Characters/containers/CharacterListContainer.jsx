8f3cd826d911eea7638b98524e7bc603b1952dbc
mohsohail <sohail.momin1@gmail.com>
2020-05-01 20:41:28 +0530

    character list component

---
+++ client/source/pages/Characters/containers/CharacterListContainer.jsx
@@ -0,0 +1,108 @@
+import React, { useContext, useState, useEffect, useLayoutEffect } from 'react';
+import { CharactersContext } from '../context/CharactersContext';
+// import List from '../../../components/List/List';
+import Pagination from '../../../components/Pagination';
+
+import '../styles/CharacterPage.scss';
+
+const Item = ({ data }) => {
+  return <div>{data.name}</div>;
+};
+
+const CharacterListContainer = (props) => {
+  const value = useContext(CharactersContext);
+  const { docs } = value;
+
+  const [charactersData] = useState(docs);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [charactersPerPage] = useState(50);
+
+  const indexOfLastCharacter = currentPage * charactersPerPage;
+  const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage;
+  const currentCharactersData = charactersData.slice(indexOfFirstCharacter, indexOfLastCharacter);
+
+  const paginate = (number) => setCurrentPage(number);
+
+  // const value = useContext(CharactersContext);
+  // const { docs } = value;
+  // const shortData = docs.slice(0, 20);
+  // const [charactersData, setCharactersData] = useState(shortData);
+  // const [direction, setDirection] = useState('asc');
+  // useEffect(() => {
+  //   setCharactersData((prevCharactersData) =>
+  //     prevCharactersData.sort((a, b) => {
+  //       const isReversed = direction === 'asc' ? 1 : -1;
+  //       return isReversed * a.name.localeCompare(b.name);
+  //     })
+  //   );
+  // }, [direction]);
+  // return (
+  //   <>
+  //     <div>
+  //       <button onClick={() => setDirection('asc')}>asc</button>
+  //       <button onClick={() => setDirection('dec')}>des</button>
+  //     </div>
+  //     <table>
+  //       <thead align="left">
+  //         <tr>
+  //           <th>Name</th>
+  //           <th>Race</th>
+  //           <th>Realm</th>
+  //           <th>Birth</th>
+  //           <th>Death</th>
+  //         </tr>
+  //       </thead>
+  //       <tbody>
+  //         {charactersData.map((item) => {
+  //           return (
+  //             <tr key={item._id}>
+  //               <td>{item.name}</td>
+  //               <td>{item.race}</td>
+  //               <td>{item.realm}</td>
+  //               <td>{item.birth}</td>
+  //               <td>{item.death}</td>
+  //             </tr>
+  //           );
+  //         })}
+  //       </tbody>
+  //     </table>
+  //   </>
+  // );
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Race</th>
+            <th>Realm</th>
+            <th>Birth</th>
+            <th>Death</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentCharactersData.map((item) => {
+            return (
+              <tr key={item._id}>
+                <td>{item.name}</td>
+                <td>{item.race}</td>
+                <td>{item.realm}</td>
+                <td>{item.birth}</td>
+                <td>{item.death}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {/* <List data={currentCharactersData} renderItem={(item) => <Item data={item} />} /> */}
+      <Pagination
+        itemsPerPage={charactersPerPage}
+        totalItems={charactersData.length}
+        paginate={paginate}
+      />
+    </>
+  );
+};
+
+export default CharacterListContainer;
---
+++ client/source/pages/Characters/styles/CharacterPage.styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { background1 } from '../../../constants/styles';
+
+export const Page = styled.div``;
+export const ContainerTwoColumn = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  margin: 30px 60px;
+`;
+
+export const ContainerThreeColumn = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  margin: 30px 60px;
+`;
+
+export const LeftSection = styled.div`
+  grid-area: 'left';
+  /* height: 500px; */
+`;
+export const MiddleSection = styled.div`
+  grid-area: 'middle';
+  /* height: 100%; */
+`;
+export const RightSection = styled.div`
+  grid-area: 'right';
+  background-color: ${background1};
+  /* height: 500px; */
+  /* border-radius: 6px; */
+  margin-left: 40px;
+`;
+
+export const Table = styled.table`
+  border: 1px solid;
+`;

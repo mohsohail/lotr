560c32eeaf3bcdad4465ff1262ad4d13b02e168d
mohsohail <sohail.momin1@gmail.com>
2020-05-02 00:04:56 +0530

    custom error handling for books page

--- client/source/pages/Books/views/BooksView.jsx
+++ client/source/pages/Books/views/BooksView.jsx
@@ -9,27 +9,34 @@ import CharactersListContainer from '../containers/CharactersListContainer';
 
 import { Page, Container, LeftSection, MiddleSection, RightSection } from '../styles/BookPage';
 
+import ErrorBoundary from '../../../errors/ErrorBoundary/index';
+
 const BookView = (props) => {
   const { booksData, bookData, mainCharacters } = props;
   return (
-    <Page>
-      <Header></Header>
-      <Container>
-        <LeftSection>
-          {booksData.status === 2 && (
-            <BooksListContainer booksData={booksData} handleFetchBook={props.handleFetchBook} />
-          )}
-        </LeftSection>
-        <MiddleSection>
-          {bookData.status === 2 && <BookDetailsContainer bookData={bookData} />}
-        </MiddleSection>
-        <RightSection>
-          {mainCharacters && mainCharacters.length !== 0 && (
-            <CharactersListContainer mainCharactersData={mainCharacters} history={props.history} />
-          )}
-        </RightSection>
-      </Container>
-    </Page>
+    <ErrorBoundary renderItem={() => <div>Custom error message for Books Page</div>}>
+      <Page>
+        <Header></Header>
+        <Container>
+          <LeftSection>
+            {booksData.status === 2 && (
+              <BooksListContainer booksData={booksData} handleFetchBook={props.handleFetchBook} />
+            )}
+          </LeftSection>
+          <MiddleSection>
+            {bookData.status === 2 && <BookDetailsContainer bookData={bookData} />}
+          </MiddleSection>
+          <RightSection>
+            {mainCharacters && mainCharacters.length !== 0 && (
+              <CharactersListContainer
+                mainCharactersData={mainCharacters}
+                history={props.history}
+              />
+            )}
+          </RightSection>
+        </Container>
+      </Page>
+    </ErrorBoundary>
   );
 };
 
--- client/source/pages/Characters/CharacterPage.jsx
+++ client/source/pages/Characters/CharacterPage.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// importing context Provider
 import { CharactersProvider } from './context/CharactersContext';
 
+// import components here
 import CharactersView from './views/CharactersView';
+
+import ErrorBoundary from '../../errors/ErrorBoundary/index';
 
 const CharacterPage = (props) => {
   return (
     <CharactersProvider>
-      <CharactersView />
+      <ErrorBoundary renderItem={() => <div>Custom error message for Character Page</div>}>
+        <CharactersView />
+      </ErrorBoundary>
     </CharactersProvider>
   );
 };

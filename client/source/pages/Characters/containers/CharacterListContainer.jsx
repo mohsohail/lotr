a98a0e94661b45b36196bdd8270c7dd101a35655
mohsohail <sohail.momin1@gmail.com>
2020-05-08 15:38:40 +0530

    remove dead code

--- client/source/pages/Characters/containers/CharacterListContainer.jsx
+++ client/source/pages/Characters/containers/CharacterListContainer.jsx
@@ -23,52 +23,6 @@ const CharacterListContainer = (props) => {
 
   const paginate = (number) => setCurrentPage(number);
 
-  // const value = useContext(CharactersContext);
-  // const { docs } = value;
-  // const shortData = docs.slice(0, 20);
-  // const [charactersData, setCharactersData] = useState(shortData);
-  // const [direction, setDirection] = useState('asc');
-  // useEffect(() => {
-  //   setCharactersData((prevCharactersData) =>
-  //     prevCharactersData.sort((a, b) => {
-  //       const isReversed = direction === 'asc' ? 1 : -1;
-  //       return isReversed * a.name.localeCompare(b.name);
-  //     })
-  //   );
-  // }, [direction]);
-  // return (
-  //   <>
-  //     <div>
-  //       <button onClick={() => setDirection('asc')}>asc</button>
-  //       <button onClick={() => setDirection('dec')}>des</button>
-  //     </div>
-  //     <table>
-  //       <thead align="left">
-  //         <tr>
-  //           <th>Name</th>
-  //           <th>Race</th>
-  //           <th>Realm</th>
-  //           <th>Birth</th>
-  //           <th>Death</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         {charactersData.map((item) => {
-  //           return (
-  //             <tr key={item._id}>
-  //               <td>{item.name}</td>
-  //               <td>{item.race}</td>
-  //               <td>{item.realm}</td>
-  //               <td>{item.birth}</td>
-  //               <td>{item.death}</td>
-  //             </tr>
-  //           );
-  //         })}
-  //       </tbody>
-  //     </table>
-  //   </>
-  // );
-
   return (
     <>
       <table>
